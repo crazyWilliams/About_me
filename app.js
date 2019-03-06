ba0ddebbc97e95173a6bf7d0f397ed66d72620c4
Williams Argenal <williamsargenal@gmail.com>
2019-03-05 17:37:48 -0800

    Lab2week1

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var question1;
+question1 = prompt ('Is Williams a code Fellow student?')
+if (question1 === 'yes'){
+    alert("Correct");
+}
+else { (question1 === 'no') 
+    alert ("Nah buddy");
+}
+console.log(question1);
+
+
+var sleep;
+sleep = prompt ('Did he have a goodnight sleep?')
+if (sleep === 'yes'){
+    alert("You bet I did");
+}
+else { (sleep === 'no') 
+    alert ("Nahhhh, I actually did");
+}
+console.log(sleep);
+
+
+var game;
+ game = prompt ('Is playstion the right console?') 
+ if (game === 'yes'){
+    alert("LETTTSSSS GOOOO!");
+}
+else { (game === 'no') 
+    alert ("next question");
+}
+
+console.log(game);
+
+var wife;
+wife = prompt ("Is he married to the hottest chick in the world")
+if (wife === 'yes'){
+    alert("Yesssss sir");
+}
+else { (wife === 'no') 
+    alert ("Quit playing with me");
+}
+console.log(wife);
+
+var last;
+last = prompt ('Is learning here fun? ')
+if (last === 'yes'){
+    alert("It really is");
+}
+else { (last === 'no') 
+    alert ("Get the strap");
+}
+console.log(last);
